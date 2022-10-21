7f76a2b677d4e9a2f7a5beed3658d4926fdc308f
Andronick Martusheff <andronick@outlook.com>
2022-10-21 16:49:18 -0700

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Andronick Martusheff on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
